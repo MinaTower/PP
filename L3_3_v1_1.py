c39518e73783018bfff4444ee58eef13f90d9c2d
MinaTower <116684345+MinaTower@users.noreply.github.com>
2023-12-14 20:35:49 +0300

    Последняя строка файла
    Корректное избавление от '\n'

--- L3_3_v1_1.py
+++ L3_3_v1_1.py
@@ -2,17 +2,18 @@ import re
 flag = int(0)
 reg = r'1([01]+)'
 with open('inp_file.txt', 'r') as file:
-	for line in file:
-		strok = line
-		while ((re.search(reg, strok) != None) and not flag):
-			m = re.search(reg, strok)
-			de = strok[int(m.start()): int(m.end())]
-			for i in range (2, len(de)+1):
-				a = int(de[:i], 2)
-				if (a%3 == 0):
-					flag = True
-					break;
-			strok = strok[:m.start()]+strok[m.end():]
-			
-		if flag: print(line[:-1])#
-		flag = False
+    for line in file:
+        strok = line
+        while ((re.search(reg, strok) != None) and not flag):
+            m = re.search(reg, strok)
+            de = strok[int(m.start()): int(m.end())]
+            for i in range (2, len(de)+1):
+                a = int(de[:i], 2)
+                if (a%3 == 0):
+                    flag = True
+                    break;
+                strok = strok[:m.start()]+strok[m.end():]
+        if flag:
+            if line[-1] == '\n': line = line[:-1]
+            print(line)#
+        flag = False
